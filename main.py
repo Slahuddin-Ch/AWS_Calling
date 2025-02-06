a11b39e80e7b732dcc38a71fc7e8b6c5fc7c7305
Abdullah Cheema <dev.edgec.io@gmail.com>
2025-02-06 16:37:04 +0500

    Modified the git account + added a user

--- main.py
+++ main.py
@@ -44,6 +44,8 @@ def get_user_data(phone_number):
         return json.dumps({"name":"John", "phone_number":"+18053078694", "active_subscription":["Upsel", "Legal"]})
     elif str(phone_number) == '+923364589301':
         return json.dumps({"name":"Sam", "phone_number":"+923364589301", "active_subscription":["Legal"]})
+    elif str(phone_number) == '+923114663661':
+        return json.dumps({"name":"Michael", "phone_number":"+923114663661", "active_subscription":["Legal", "Upsel"]})
     else:
         return json.dumps({"name":"Dani", "phone_number":"12345", "active_subscription":["Legal", "Upsel"]})
 
